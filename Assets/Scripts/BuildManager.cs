266c65fe5db6727c0dd030bf108e2e1ee5dafed7
Oliver Ganz <oli@testingtime.com>
2020-04-04 20:01:18 +0200

    Add functionality to place new turrets

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+    public static BuildManager instance;
+
+    public GameObject standardTurretPrefab;
+
+    private GameObject turretToBuild;
+
+    void Awake () {
+        if (instance != null) {
+            Debug.Log ("More than one BuildManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+    void Start () {
+        this.turretToBuild = standardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild () {
+        return turretToBuild;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+    private Renderer nodeRenderer;
+    private Color defaultColor;
+
+    void Start () {
+        nodeRenderer = GetComponent<Renderer> ();
+        defaultColor = nodeRenderer.material.color;
+    }
+    void OnMouseDown () {
+        if (turret != null) {
+            Debug.Log ("Can't build here!");
+            return;
+        }
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject) Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    void OnMouseEnter () {
+        nodeRenderer.material.color = hoverColor;
+    }
+
+    void OnMouseExit () {
+        nodeRenderer.material.color = defaultColor;
+    }
+}
