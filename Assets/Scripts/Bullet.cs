6f2193de197821f37b2d8c29b52ac90f7a8974a8
Oliver Ganz <oli@testingtime.com>
2020-04-05 14:48:53 +0200

    Added missile and its animation on explosion

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
     public float speed = 70f;
+    public float explosionRadius = 0f;
     public GameObject impactEffect;
 
     private Transform target;
@@ -27,12 +28,39 @@ public class Bullet : MonoBehaviour {
             return;
         }
         transform.Translate (dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt (target);
     }
 
     void HitTarget () {
         GameObject effect = Instantiate (impactEffect, transform.position, transform.rotation);
-        Destroy (effect, 2f);
+        Destroy (effect, 5f);
+
+        if (explosionRadius > 0f) {
+            Explode ();
+        } else {
+            Damage (target);
+        }
+
         Destroy (target.gameObject);
         Destroy (gameObject);
+    }
+
+    void Explode () {
+        Collider[] colliders = Physics.OverlapSphere (transform.position, explosionRadius);
+        foreach (Collider collider in colliders) {
+            if (collider.tag == "Enemy") {
+                Damage (collider.transform);
+            }
+        }
+
+    }
+
+    void Damage (Transform enemy) {
+        Destroy (enemy.gameObject);
+    }
+
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
